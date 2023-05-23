3f6df6ee30f1f6af02cc76a815243022557b88df
dthcs <dthcsc@gmail.com>
2023-05-24 07:03:42 +0900

    update note

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = "databse.db"
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '123456'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite://{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+
+    
 
     from .views import views
     from .auth import auth
@@ -18,13 +21,25 @@ def create_app():
     app.register_blueprint(auth, url_prefix='/')
     
     from .models import User, Note
-    create_database(app)
+
+    with app.app_context():
+        db.create_all()
+
+    # create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
 
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print('Created Database!')
+# def create_database(app):
+#     if not path.exists('website/' + DB_NAME):
+#         db.create_all(app=app)
+#         print('Created Database!')
 
 #1h34ph
